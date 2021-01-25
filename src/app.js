ef663a5e8f11aaa308d93f1596d6d4690de38e6a
thiago <thiago.lopes.dev@gmail.com>
2021-01-24 21:52:10 -0300

    fix: update routes of app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
-const http = require('http');
+const express = require('express');
 const os = require('os');
 
-console.log("Kubia server starting...");
 
-var handler = function(request, response) {
-    console.log(`Received request from ${request.connection.remoteAddress}`);
-    response.writeHead(200);
-    response.end(`You've hit ${os.hostname()} \n`);
-}
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
-var www = http.createServer(handler);
-www.listen(8080);
+console.log(`Kubia server starting...`)
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  let ip = req.connection.remoteAddress
+  res.status(200).send(`Received request from ${ip} and you've hit ${os.hostname()}`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
